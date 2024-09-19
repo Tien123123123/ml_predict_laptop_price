6ac07482235b39d1d64a0d1af2a1288fac5504ce
Tien123123123 <96216534+Tien123123123@users.noreply.github.com>
2024-09-19 15:09:51 +0700

    Add files via upload

---
+++ predict_web.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+with open("model_filename.pkl", 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/predict', mothods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)
+    return jsonify({f"Predicted Price": prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
