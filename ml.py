4ecb983de8c8a2c9d116a0776f7e87f0f6e36e4d
Tien123123123 <96216534+Tien123123123@users.noreply.github.com>
2024-09-26 11:16:57 +0700

    Update ml.py

--- ml.py
+++ ml.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 import pickle
-from lazypredict.Supervised import LazyRegressor
 
 def split_memories(loc):
     s = loc.split(" + ")
@@ -31,7 +30,7 @@ def split_screen(loc):
     number = [part.split("x") for part in s if "x" in part][0]
     return number[0], number[1]
 
-root = "laptop_price - dataset.csv"
+root = "../data/dataset.csv"
 df = pd.read_csv(root)
 drop_features = ["TypeName", "OpSys", "CPU_Company", "Memory", "ScreenResolution", "GPU_Company", "Product"]
 df[["SSD_GB", "HDD_GB", "Flash Storage_GB"]] = df["Memory"].apply(lambda x:split_memories(x)).apply(pd.Series)
@@ -43,6 +42,7 @@ x = df.drop(target, axis=1)
 y = df[target]
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=43)
 
+# Data Preprocessing
 # Numeric: Inches, Screen_Width, Screen_Height, CPU_Frequency (GHz), RAM (GB), SSD_GB, HDD_GB, Flash Storage_GB, Weight (kg)
 # Norminal: Product, CPU_Type, GPU_Type
 # Ordinal:
@@ -60,29 +60,34 @@ preprocessing_data = ColumnTransformer([
     ("norminal", norminal_transform, ["CPU_Type", "GPU_Type"])
 ])
 
+# Fine-Tuning and Deploy Model
 param = {
-    # 'n_estimators': [50, 100, 150, 200],  # Số lượng cây
-    # 'max_depth': [None, 10, 15, 20, 25],  # Độ sâu của cây
-    # 'min_samples_split': [2, 5, 10],  # Số mẫu tối thiểu để chia nút
-    # 'min_samples_leaf': [1, 2, 4, 6],  # Số mẫu tối thiểu ở mỗi lá
-    # 'max_features': ['auto', 'sqrt', 'log2']  # Số đặc trưng được xem xét để chia
+    'n_estimators': [50, 100, 150, 200],
+    'max_depth': [None, 10, 15, 20, 25]
 }
 model_reg = Pipeline(steps=[
     ("processing", preprocessing_data),
-    # ("model", GridSearchCV(RandomForestRegressor(random_state=123), param_grid=param, verbose=2, cv=6,scoring='r2'))
-    ("model", RandomForestRegressor(random_state=123))
+    ("model", GridSearchCV(RandomForestRegressor(random_state=123), param_grid=param, verbose=2, cv=6,scoring='r2'))
+    # ("model", RandomForestRegressor(random_state=123))
 ])
 model_reg.fit(x_train, y_train)
+
+# Grid Search Result
+grid_search = model_reg.named_steps['model']
+print("Best Parameters:", grid_search.best_params_)
+print("Best Estimator:", grid_search.best_estimator_)
+
 y_pred = model_reg.predict(x_test)
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
-print(f"MSE: {mse}, MAE: {mae}")
-print(f"R2 score: {r2_score(y_test, y_pred)}")
+r2 = r2_score(y_test, y_pred)
+print(f"MSE: {mse}, MAE: {mae}, R2 Score:{r2}")
 
 # Save the model to a file
-with open('model.pkl', 'wb') as file:
+with open('model_filename.pkl', 'wb') as file:
     pickle.dump(model_reg, file)
 
+# Visualize Predictions
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(
     range(len(y_test)),
