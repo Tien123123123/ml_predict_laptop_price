a4d3e11c5f19ada44374666e0f4451702c59634e
Tien123123123 <96216534+Tien123123123@users.noreply.github.com>
2024-09-26 11:46:50 +0700

    Update ml.py

--- ml.py
+++ ml.py
@@ -6,6 +6,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.feature_selection import SelectKBest, f_regression
 import pickle
 
 def split_memories(loc):
@@ -30,7 +31,7 @@ def split_screen(loc):
     number = [part.split("x") for part in s if "x" in part][0]
     return number[0], number[1]
 
-root = "../data/dataset.csv"
+root = "C:/Users/Dell/Documents/AI/Python/Datasets/laptop_price - dataset.csv"
 df = pd.read_csv(root)
 drop_features = ["TypeName", "OpSys", "CPU_Company", "Memory", "ScreenResolution", "GPU_Company", "Product"]
 df[["SSD_GB", "HDD_GB", "Flash Storage_GB"]] = df["Memory"].apply(lambda x:split_memories(x)).apply(pd.Series)
@@ -41,8 +42,7 @@ target = "Price (Euro)"
 x = df.drop(target, axis=1)
 y = df[target]
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=43)
-
-# Data Preprocessing
+# Data preprocessing
 # Numeric: Inches, Screen_Width, Screen_Height, CPU_Frequency (GHz), RAM (GB), SSD_GB, HDD_GB, Flash Storage_GB, Weight (kg)
 # Norminal: Product, CPU_Type, GPU_Type
 # Ordinal:
@@ -59,31 +59,31 @@ preprocessing_data = ColumnTransformer([
     ("numeric", numeric_transform, ["Inches", "Screen_Width", "Screen_Height", "CPU_Frequency (GHz)", "RAM (GB)", "SSD_GB", "HDD_GB", "Flash Storage_GB", "Weight (kg)"]),
     ("norminal", norminal_transform, ["CPU_Type", "GPU_Type"])
 ])
-
-# Fine-Tuning and Deploy Model
+# Grid Search
 param = {
-    'n_estimators': [50, 100, 150, 200],
-    'max_depth': [None, 10, 15, 20, 25]
+    'n_estimators': [50, 100, 150, 200, 250, 300],
+    'max_depth': [15, 20, 25, 30],
 }
 model_reg = Pipeline(steps=[
     ("processing", preprocessing_data),
+    ("feature_selection", SelectKBest(score_func=f_regression, k=20)),
     ("model", GridSearchCV(RandomForestRegressor(random_state=123), param_grid=param, verbose=2, cv=6,scoring='r2'))
     # ("model", RandomForestRegressor(random_state=123))
 ])
 model_reg.fit(x_train, y_train)
 
-# Grid Search Result
+# Metrics and estimator
 grid_search = model_reg.named_steps['model']
-print("Best Parameters:", grid_search.best_params_)
 print("Best Estimator:", grid_search.best_estimator_)
-
+print("Best Score:", grid_search.best_score_)
 y_pred = model_reg.predict(x_test)
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-print(f"MSE: {mse}, MAE: {mae}, R2 Score:{r2}")
+print(f"MSE: {mse}, MAE: {mae}, R2 Score: {r2}")
 
-# Save the model to a file
+
+# Save the model
 with open('model_filename.pkl', 'wb') as file:
     pickle.dump(model_reg, file)
 
